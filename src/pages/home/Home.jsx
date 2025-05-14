72c2b24599eb4ce648a93dfabd758a4015f84e85
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-05-14 23:06:55 +0600

    hero section has been designed here

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PopularCoffees from '../../components/popularCoffee/PopularCoffees';
 import FollowUs from '../../components/followUs/FollowUs';
+import Hero from '../../components/hero/Hero';
 
 
 const Home = () => {
     return (
         <div className='space-y-8 md:space-y-16'>
+            <Hero/>
             <PopularCoffees />
             <FollowUs/>
         </div>
